e27389bcc521b5da74f260b595fea306e6b2fc64
Quentin Chevrin <qchevrin@pictet.com>
2020-08-28 13:47:13 +0200

    initial page design

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  password = '';
+
+  updatePassword = (event) => {
+    this.password = event.target.value
+  }
+
+  triggerDownload = () => {
+    console.log(this.password)
+  }
 }
